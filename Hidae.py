0c77c2b5c53cfd8af7f9caed27ead4e900b726f9
Stephanos23 <stephen.luu2000@hotmail.com>
2020-06-05 22:58:35 -0400

    init

--- Hidae.py
+++ Hidae.py
@@ -4,11 +4,9 @@ from io import BytesIO
 import os
 import discord
 
-discordToken = ""
-lounge_channel =
-member_count_channel =
-guild_id =
-#make welcome banner
+headerMessage = "Welcome"
+
+#Make welcome banner
 def bannerMake(avUrl,userName):
     #get profile picture
     url = avUrl
@@ -17,7 +15,7 @@ def bannerMake(avUrl,userName):
     pfpImg = Image.open(BytesIO(response.content))
     pfpImg = pfpImg.resize((128, 128));
 
-    #crop circle
+    #crop circular pfp
     bigsize = (pfpImg.size[0] * 3, pfpImg.size[1] * 3)
     mask = Image.new('L', bigsize, 0)
     draw = ImageDraw.Draw(mask)
@@ -27,7 +25,7 @@ def bannerMake(avUrl,userName):
     output = ImageOps.fit(pfpImg, mask.size, centering=(0.5, 0.5))
     output.putalpha(mask)
 
-    #add image to bg_ file
+    #add pfp to background
     background = Image.open('bg_temp.jpg')
     background = background.resize((background.width // 2, background.height // 2))
     background.paste(pfpImg,((background.width // 15)*6, background.height // 4), pfpImg)
@@ -45,8 +43,9 @@ def bannerMake(avUrl,userName):
     draw.text((x, y+1), name, font=font, fill="black")
     draw.text((x, y), name, font=font, fill="white")
     #background.show()
+
     #Header
-    header = "Welcome to Hidden!"
+    header = headerMessage
     font = ImageFont.truetype('Fonts/arialbd.ttf', 36)
     w, h = draw.textsize(header, font=font)
     x, y = ((W-w)/2,(H-h)/8)
@@ -62,26 +61,55 @@ def bannerMake(avUrl,userName):
     else:
         background.save('output.png')
 
-#discord bot code
+#Discord Py
 client = discord.Client()
 
+#Config
+"""
+discordToken = ""
+channel_id =
+count_channel_id =
+guild_id =
+"""
+#Heroku
+discordToken = os.environ['TOKEN']
+channel_id = os.environ['CHANNELID']
+count_channel_id = os.environ['COUNTID']
+guild_id = os.environ['GUILDID']
+
+#Bot ready
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
+#Member Join event
 @client.event
 async def on_member_join(member):
     url = member.avatar_url
-    name = member.name
+    name = member.name + "#" + member.discriminator
+    print("User %s Joined"%name)
     bannerMake(url,name)
-    channel = client.get_channel(lounge_channel)
+    channel = client.get_channel(channel_id)
     await channel.send(file=discord.File('output.png'))
+    #Delete output file for welcome banner
     if os.path.exists('output.png'):
         os.remove('output.png')
     else:
         print('No output file')
     guild = client.get_guild(guild_id)
     total_members = guild.member_count
-    member_count = client.get_channel(member_count_channel)
-    await member_count.edit(name="Member Count: %d"%total_members)
+    count_channel = client.get_channel(count_channel_id)
+    await count_channel.edit(name="Member Count: %d"%total_members)
+    print("Updating Count Channel")
+#Member Leave event
+@client.event
+async def on_member_remove(member):
+    name = member.name
+    print("User %s Left"%name)
+    guild = client.get_guild(guild_id)
+    total_members = guild.member_count
+    count_channel = client.get_channel(count_channel_id)
+    await count_channel.edit(name="Member Count: %d"%total_members)
+    print("Updating Count Channel")
+#Run bot
 client.run(discordToken)
